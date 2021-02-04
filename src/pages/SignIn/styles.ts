e9f624ab5d0804dd1c31caf62ad57637fc8b861a
Marlon Branco <marlonaparecido18@gmail.com>
2021-02-04 18:05:41 -0300

    chore: Página de SignUp finalizada e navegação feita

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,15 +1,15 @@
-import {Text, View} from 'react-native';
+import {Platform} from 'react-native';
 import styled from 'styled-components/native';
 import {getBottomSpace} from 'react-native-iphone-x-helper';
 
-export const Container = styled(View)`
+export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 30px;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
 `;
 
-export const Title = styled(Text)`
+export const Title = styled.Text`
   font-family: 'RobotoSlab-Medium';
   font-size: 24px;
   color: #f4ede8;
