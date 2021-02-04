ef09779219571f5064ca2ebd9cb40ab1693514a8
Marlon Branco <marlonaparecido18@gmail.com>
2021-02-03 21:32:25 -0300

    chore: Configuracao inicial das rotas criada

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,15 @@
+import {Text, View} from 'react-native';
+import styled from 'styled-components';
+
+export const Container = styled(View)`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled(Text)`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 24px;
+  color: #f4ede8;
+  margin: 64px 0 24px;
+`;
